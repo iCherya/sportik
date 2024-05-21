28d83afa6d05820b58d404b57bc212efde1bbf75
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2024-05-21 23:44:29 +0300

    chore: update linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,17 @@ module.exports = {
   plugins: ['prettier'],
   rules: {
     'prettier/prettier': 'error',
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal', ['parent', 'sibling'], 'index', 'unknown'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
   },
   env: {
     jest: true,
